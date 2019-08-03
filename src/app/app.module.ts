d13f1d957b20a179548e5afec48d7a6fefd9d842
Ana Ribeiro <ribeiro.anacb@gmail.com>
2019-08-03 00:23:01 -0300

    Including routes application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,31 +2,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { ConfigService } from './config/config.service';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule,Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { AdvancedComponent } from './advanced.component';
+import { HistoryComponent } from './history.component';
 
-const routes: Routes = [
-    {
-        path: '', component: AppComponent, pathMatch: 'prefix',
-        children: [
-            {path: 'medias', component: AdvancedComponent}
-        ]
-    },
+
+const appRoutes: Routes = [
+  { path: 'medias', component: AdvancedComponent },
+  { path: 'taxas',        component: HistoryComponent },
+  { path: '',   redirectTo: '/taxas', pathMatch: 'full' },
+  { path: '**', component: AppComponent }
 ];
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdvancedComponent
+    AdvancedComponent,
+    HistoryComponent
   ],
   imports: [
     HttpClientModule,
-    RouterModule.forRoot(routes),
+    RouterModule.forRoot(appRoutes),
     BrowserModule
   ],
+  exports: [
+    RouterModule
+  ],
   providers: [ConfigService],
-  bootstrap: [AppComponent], entryComponents: [AdvancedComponent]
+  bootstrap: [AppComponent], entryComponents: [AdvancedComponent, HistoryComponent]
 })
 export class AppModule { }
